b932d5875372b68c19fd5fe198af1b871fd4e83e
Erik Fasterius <erikfas@kth.se>
2017-10-24 10:05:55 +0200

    Add functionality to loop through SE/PE samples
    Add functionality in the snakemake wrapper to loop through single- and
    paired-end samples, one at a time, creating two separate workflows
    through the single wrapper.

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Run snakemake
-snakemake \
-    --snakefile snakefile \
-    --jobs 20 \
-    --keep-going \
-    --cluster-config cluster.yaml \
-    --cluster "sbatch \
-                  --account {cluster.account} \
-                  --time {cluster.time} \
-                  --partition {cluster.partition} \
-                  --ntasks {cluster.ntasks} \
-                  --mail-type {cluster.mail_type} \
-                  --job-name {cluster.job_name}"
+# Loop over single and paired-end reads
+for CURRENT_LAYOUT in SINGLE PAIRED; do
+
+    # Run snakemake
+    snakemake \
+        --snakefile snakefile \
+        --config LAYOUT=$CURRENT_LAYOUT \
+        --jobs 20 \
+        --keep-going \
+        --cluster-config cluster.yaml \
+        --cluster "sbatch \
+                      --account {cluster.account} \
+                      --time {cluster.time} \
+                      --partition {cluster.partition} \
+                      --ntasks {cluster.ntasks} \
+                      --mail-type {cluster.mail_type} \
+                      --job-name {cluster.job_name}"
+done
